566d9f80a56da59d3e24f6d1847f33add288edcb
Suryansh Gupta <suryanshgupta9933@gmail.com>
2025-02-05 01:48:23 +0530

    streamlit app

---
+++ app.py
@@ -0,0 +1,29 @@
+# Importingf Dependencies
+import streamlit as st
+
+from src.pipeline import summarize_text, detect_emotion, search_book, count_words
+
+def app():
+    st.title("Assignment")
+    st.markdown("This app analyzes the book passage and provides insights on the same.")
+
+    st.markdown("Please upload the book passage in the text box below.")
+    text = st.text_area("Enter Text")
+
+    if st.button("Analyze"):
+        with st.spinner("Analyzing the text..."):
+            summary = summarize_text(text)
+            st.markdown("### Summary")
+            st.write(summary)
+            emotion = detect_emotion(text)
+            st.markdown("### Emotion")
+            st.write(emotion)
+            book = search_book(text)
+            st.markdown("### Book")
+            st.write(book)
+            word_count = count_words(text)
+            st.markdown("### Word Count")
+            st.write(word_count)
+
+if __name__ == "__main__":
+    app()
